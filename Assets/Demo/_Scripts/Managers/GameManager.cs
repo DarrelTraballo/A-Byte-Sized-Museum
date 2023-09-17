e30115b1d0d95c3486b6f83c4e6cd7e1248ca914
Darrel Traballo <barreltraballo1121@gmail.com>
2023-09-17 15:12:14 +0800

    Added Level 1 Room and player with player controller

--- Assets/Demo/_Scripts/Managers/GameManager.cs
+++ Assets/Demo/_Scripts/Managers/GameManager.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Video;
 
-public class Test : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
