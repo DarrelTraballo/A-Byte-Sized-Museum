3a5029f60c25af13109e4a1d868a6bd4d1765f71
Angelica Rose Gumanid <84778428+AngelicaGumanid@users.noreply.github.com>
2024-02-27 01:27:07 +0800

    Music
    Theme Music
    Fixing Toggle and SFX Sounds

---
+++ Assets/UIController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KaChow.AByteSizedMuseum
+{
+    public class UIController : MonoBehaviour
+    {
+        public Slider _musicSlider, _sfxSlider;
+
+        public void ToggleMusic()
+        {
+            AudioManager.Instance.ToggleMusic();
+        }
+
+        public void ToggleSFX()
+        {
+            AudioManager.Instance.ToggleSFX();
+        }
+
+        public void MusicVolume()
+        {
+            AudioManager.Instance.MusicVolume(_musicSlider.value);
+        }
+
+        public void SFXVolume()
+        {
+            AudioManager.Instance.SFXVolume(_sfxSlider.value);
+        }
+    }
+}
