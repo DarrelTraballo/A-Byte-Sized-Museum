ef33dd1d096f550a34a68cdad0af1f7f6556f0fc
Darrel Traballo <barreltraballo1121@gmail.com>
2024-05-25 01:11:49 +0800

    tried something for inserting icons in dialogue system
    (idk if any of these work yet)

--- Assets/A Byte Sized Museum/_Scripts/_Managers/Dialogue/DialogueSO.cs
+++ Assets/A Byte Sized Museum/_Scripts/_Managers/Dialogue/DialogueSO.cs
@@ -11,6 +11,23 @@ namespace KaChow.AByteSizedMuseum
     {
         public TextAsset dialogueFile;
         public Dialogue dialogue;
+        public Dictionary<string, Sprite> keyboardIcons; // Dictionary to hold keyboard icons
+
+        // private void OnEnable()
+        // {
+        //     InitializeKeyboardIcons();
+        // }
+
+        // private void InitializeKeyboardIcons()
+        // {
+        //     // Assuming you have a folder named "KeyboardIcons" in your Resources folder with all icons named after the key they represent
+        //     keyboardIcons = new Dictionary<string, Sprite>();
+        //     Sprite[] icons = Resources.LoadAll<Sprite>("KeyboardIcons");
+        //     foreach (Sprite icon in icons)
+        //     {
+        //         keyboardIcons.Add(icon.name, icon);
+        //     }
+        // }
 
         public void LoadDialogueFromFile()
         {
@@ -26,10 +43,31 @@ namespace KaChow.AByteSizedMuseum
                     var linesList = allLines.ToList();
                     linesList.RemoveAt(0);
                     dialogue.sentences = linesList.ToArray();
+                    // dialogue.sentences = linesList.Select(line => ParseLine(line)).ToArray();
                 }
 
             }
             else Debug.LogError("Dialogue File not assigned.");
         }
+
+        // private object[] ParseLine(string line)
+        // {
+        //     List<object> parsedLine = new List<object>();
+        //     var segments = Regex.Split(line, "&(.*?)&");
+
+        //     foreach (var segment in segments)
+        //     {
+        //         if (keyboardIcons.ContainsKey(segment))
+        //         {
+        //             parsedLine.Add(keyboardIcons[segment]); // Add the icon sprite
+        //         }
+        //         else
+        //         {
+        //             parsedLine.Add(segment); // Add the regular text
+        //         }
+        //     }
+
+        //     return parsedLine.ToArray();
+        // }
     }
 }
